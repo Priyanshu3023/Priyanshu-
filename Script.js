82b7fa4691182a3e5e886645b6059ec8feca950f
Priyanshu3023 <157918602+Priyanshu3023@users.noreply.github.com>
2024-01-29 00:21:30 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,12 @@
+function login() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  // Perform login logic (you can add your own authentication logic here)
+
+  console.log("Login attempt with username:", username);
+}
+
+function openRegisterForm() {
+  // Handle registration link click event (if needed)
+}
